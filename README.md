3c1c09a5e4a705308fa84dfe27084f93e0e6e803
Victor Silva <contact@victorleonardo.com>
2016-04-12 15:54:41 -0300

    Version 1.0.0

---
+++ README.md
@@ -0,0 +1,33 @@
+# jQuery Route
+
+<a href="https://raw.githubusercontent.com/victorlss/jquery-route/jquery.route.js" target="_blank">Download</a>
+
+##### Example:
+```html
+<div id="content"></div>
+
+<script type="text/javascript" src="jquery.route.js"></script>
+<script type="text/javascript">
+  $("#content").route({
+    default: "home",
+    routes: [
+      {
+        url: "home",
+        view: "views/home.html"
+      },
+      {
+        url: "users",
+        view: "views/users/index.html"
+      },
+      {
+        url: "users/add",
+        view: "views/users/add.html"
+      }
+    ]
+  });
+</script>
+```
+
+Authors: <a href="https://github.com/victorlss" target="_blank">Victor Silva</a>
+All Rights Reserved.<br />
+Use, reproduction, distribution, and modification of this code is subject to the terms and conditions of the MIT license, available at <a href="http://www.opensource.org/licenses/mit-license.php" target="_blank">http://www.opensource.org/licenses/mit-license.php</a>.
---
+++ jquery.route.js
@@ -0,0 +1,52 @@
+/*!
+ * jQuery Route v1.0.0
+ * 
+ * Copyright 2016
+ * Authors: Victor Silva
+ * All Rights Reserved.
+ * Use, reproduction, distribution, and modification of this code is subject to the terms and
+ * conditions of the MIT license, available at http://www.opensource.org/licenses/mit-license.php
+ *
+ *
+ * Project: https://github.com/victorlss/jquery-route
+ */
+(function($){
+  $.fn.route = function( options ) {
+
+    var settings = $.extend({
+      default: '',
+      routes: [],
+      content: this
+    }, options );  
+
+    window.onhashchange = function(){
+      loadPage(window.location.hash);
+    }
+
+    loadPage(window.location.hash ? window.location.hash : "#" + settings.default);
+
+    function loadPage(hash)
+    {
+      var url = hash.substring(1);
+      var route;
+      
+      $.each(settings.routes,function(index,value){
+        if(value.url == url)
+          route = value;
+      });
+
+      if(route){
+        $.get(route.view).done(function() {           
+          settings.content.load(route.view);
+          document.location.hash = "#" + route.url;
+        }).fail(function(){
+          console.log("404"); //TODO: 404 page
+        });
+      }
+      else
+       console.log("404"); //TODO: 404 page
+    }
+  };
+
+  return this;
+}(jQuery));
