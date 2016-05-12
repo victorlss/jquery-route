adf405583bca9b1d590a3a12299f69988a46585f
Victor Leonardo <contact@victorleonardo.com>
2016-05-12 11:03:12 -0300

    Route with params

--- jquery.route.js
+++ jquery.route.js
@@ -28,10 +28,21 @@
     function loadPage(hash)
     {
       var url = hash.substring(1);
-      var route;
+      var route = {};
       
       $.each(settings.routes,function(index,value){
-        if(value.url == url)
+        var paramsNames = value.url.match(/\{([a-z]+)\}/g);
+        if(paramsNames){
+          var params = url.match(/([0-9]+)/g);
+          if(paramsNames.length == params.length){
+            route = value;
+            route.url = url;
+            $.each(paramsNames,function(index,name){
+              route.view = route.view.replace(name,params[index])
+            });
+          }	
+        }	
+        else if(value.url == url)
           route = value;
       });
 
